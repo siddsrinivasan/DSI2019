e8ed41fb0a553c5a026075c46e2bdbc62d1a8bfd
Sidd Srinivasan <siddsrinivasan@sidds-mbp.lan>
2019-02-16 20:13:17 +0100

    terminado con microp_2

--- microp_2/microp02/src/app/app.component.ts
+++ microp_2/microp02/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import​ { ​Component​,​Input​ } ​from​ ​'@angular/core'​; import​ { ​FormsModule​ } ​from​ ​'@angular/forms'​; import​ { ​Coche​,​EstadoCoche​ } ​from​ ​'./coche'​;
+@​Component​({
+​selector:​ ​'app-root'​,
+​templateUrl:​ ​'./app.component.html'​, ​styleUrls:​ [​'./app.component.css'​]
 })
-export class AppComponent {
-  title = 'microp02';
+export​ ​class​ ​AppComponent​ {
+​title​ = ​'Micropráctica ANGULAR - 02'​;
+​public​ ​EstadoCoche​:​any​ = ​EstadoCoche​;
+​public​ ​coches​:​Array​<​Coche​>=[ ​new​ ​Coche​(​'renault'​,​'scenic'​,​new Date​(​2007​,​10​,​1​),​new​ ​Date​(​2018​,​3​,​1​),​5000​,​'coche01.jpg'​,​EstadoCoche​.​BUENO​),
+​new​ ​Coche​(​'seat'​,​'ibiza'​,​new​ ​Date​(​2003​,​4​,​12​),​new Date​(​2018​,​2​,​1​),​1200​,​'coche02.jpg'​,​EstadoCoche​.​BUENO​),
+​new​ ​Coche​(​'renault'​,​'megane'​,​new​ ​Date​(​2006​,​12​,​23​),​new Date​(​2018​,​2​,​1​),​3500​,​'coche03.jpg'​,​EstadoCoche​.​MALO​),
+​new​ ​Coche​(​'tesla'​,​'model 3'​,​new​ ​Date​(​2006​,​12​,​23​),​new Date​(​2018​,​2​,​1​),​4000​,​''​,​EstadoCoche​.​BUENO​)];
+​public​ ​filtro​:​string​=​''​;
+​public​ ​getFiltrados​():​Array​<​number​>{ ​var​ ​filtrados​:​Array​<​number​>=[]; ​var​ ​indice​:​number​=​0​;
+​for​(​var​ ​coche​ ​of​ ​this​.​coches​)
+{
+​if​ (​coche​.​marca​.​lastIndexOf​(​this​.​filtro​, ​0​) === ​0​)
+filtrados​.​push​(​indice​); ​indice​=​indice​+​1​;
+}
+​return​ ​filtrados​;
+}
+​public​ ​cmd_vender​(​id​) {
+​this​.​coches​.​splice​(​id​,​1​); }
+​public​ ​cmd_rebajar​(​id​) {
+​this​.​coches​[​id​].​rebajar​(); }
 }
--- microp_2/microp02/src/app/coche.ts
+++ microp_2/microp02/src/app/coche.ts
@@ -1,2 +1,31 @@
+export enum EstadoCoche {BUENO, MALO}
+
 export class Coche {
+
+ public marca:string;
+ public modelo:string;
+ public fecha_modelo:Date;
+ public fecha_venta:Date;
+ public precio:number;
+ public foto:string;
+ public estado:EstadoCoche;
+constructor(marca:string,modelo:string,fecha_modelo:Date,fecha_venta:Date,precio:
+number,foto:string,estado:EstadoCoche)
+ {
+ this.marca=marca;
+ this.modelo=modelo;
+ this.fecha_modelo=fecha_modelo;
+ this.fecha_venta=fecha_venta;
+ this.precio=precio;
+ this.foto=foto;
+ this.estado=estado;
+ }
+ 
+ public getPVP():number{
+    return this.precio*1.21;
+    }
+    public rebajar(){
+    this.precio*=0.90;
+    }
+
 }
