13f9ae4584322f68e45c90e201ab6ff21c4f3a38
Sidd Srinivasan <siddsrinivasan@Sidds-MacBook-Pro.local>
2019-02-25 13:08:25 +0100

    added microp_4

---
+++ microp_4/Agenda/src/pages/libreta-contactos/libreta-contactos.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LibretaContactosPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-libreta-contactos',
+  templateUrl: 'libreta-contactos.html',
+})
+export class LibretaContactosPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LibretaContactosPage');
+  }
+
+}
