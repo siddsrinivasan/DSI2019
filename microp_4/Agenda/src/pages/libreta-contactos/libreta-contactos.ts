0d9e6dc9e26c5ae9a066a85f0614b52a20dadf9f
Sidd Srinivasan <siddsrinivasan@Sidds-MacBook-Pro.local>
2019-02-28 15:43:32 +0100

    finished project

--- microp_4/Agenda/src/pages/libreta-contactos/libreta-contactos.ts
+++ microp_4/Agenda/src/pages/libreta-contactos/libreta-contactos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import {NuevoContactoPage} from '../nuevo-contacto/nuevo-contacto';
+import { AcercaDePage } from '../acerca-de/acerca-de';
 
 /**
  * Generated class for the LibretaContactosPage page.
