9708ad1024d15d9faeeadf2ef1c73208b6da0c4c
Sidd Srinivasan <siddsrinivasan@Sidds-MacBook-Pro.local>
2019-03-07 14:27:56 +0100

    he empezado micro_p5

--- microp_4/Agenda/src/pages/libreta-contactos/libreta-contactos.ts
+++ microp_4/Agenda/src/pages/libreta-contactos/libreta-contactos.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import {NuevoContactoPage} from '../nuevo-contacto/nuevo-contacto';
 import { AcercaDePage } from '../acerca-de/acerca-de';
+import { Contact } from '../../models/contact.model';
+import { ContactService } from '../../services/contact.service';
 
 /**
  * Generated class for the LibretaContactosPage page.
@@ -18,11 +20,23 @@ import { AcercaDePage } from '../acerca-de/acerca-de';
 })
 export class LibretaContactosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  contacts: Contact[] =[];
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private ContactService:ContactService) {
+  }
+
+  ionViewWillEnter(){
+    this.contacts = this.ContactService.getContacts();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LibretaContactosPage');
+  }
+
+  onAddContact(value:
+    {nombre:string,organizacion:string,movil:string,correo:string}){
+      this.ContactService.addContact(value);
+      this.navCtrl.pop();
   }
 
   onLoadContactosPage(){
