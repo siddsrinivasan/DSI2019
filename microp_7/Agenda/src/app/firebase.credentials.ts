290b6f3763c635b7ecfd663a0362f428634e177c
Sidd Srinivasan <siddsrinivasan@Sidds-MacBook-Pro.local>
2019-03-25 16:04:58 +0100

    añadí credentials de firebase

---
+++ microp_7/Agenda/src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDDIaZUvZm5lgSh2RemEzbX4zmxrwnMe_Y",
+    authDomain: "microp-07.firebaseapp.com",
+    databaseURL: "https://microp-07.firebaseio.com",
+    projectId: "microp-07",
+    storageBucket: "microp-07.appspot.com",
+    messagingSenderId: "9629342655"
+    };
